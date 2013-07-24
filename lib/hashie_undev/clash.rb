928be1a4579c073403b5ad22b6ba3f87a7bfbe4a
Shumilkin Gena <shumilkin.gena@yandex.ru>
2013-07-24 13:50:13 +0400

    vagrant init

--- lib/hashie_undev/clash.rb
+++ lib/hashie_undev/clash.rb
@@ -1,44 +1,44 @@
 module HashieUndev
 
-	class Clash < Hash
+  class Clash < Hash
 
-		attr_accessor :is_where
+    attr_accessor :is_where
 
-		def where(*args)
-			self[:where] ||= {}
-			self[:where].merge! *args
-			self
-		end
+    def where(*args)
+      self[:where] ||= {}
+      self[:where].merge! *args
+      self
+    end
 
-		def order(*args)
-			self[:order] = args.size == 1 ? args[0] : args
-			self 
-		end
+    def order(*args)
+      self[:order] = args.size == 1 ? args[0] : args
+      self 
+    end
+    
+    def where!
+      self[:where] = {}
+      self.is_where = true
+      self
+    end
 
-		def where!
-			self[:where] = {}
-			self.is_where = true
-			self
-		end
+    def _end!
+      self.is_where = false
+      self
+    end
+    
+    def method_missing(meth, *args, &block)
+      # binding.pry
+      define_singleton_method(meth) do |value|
+        if self.is_where
+          self[:where].merge!({meth => args[0]})
+          self
+        else
+          raise NoMethodError
+        end
+      end
 
-		def _end!
-			self.is_where = false
-			self
-		end
-		
-		def method_missing(meth, *args, &block)
-			# binding.pry
-			define_singleton_method(meth) do |value|
-				if self.is_where
-					self[:where].merge!({meth => args[0]})
-					self
-				else
-					raise NoMethodError
-				end
-			end
+      send meth, *args
+    end
 
-			send meth, *args
-		end
-
-	end
+  end
 end
